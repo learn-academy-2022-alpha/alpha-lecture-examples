34d9eb78a58c74ce3e478979cfc4185fef01a21a
Valeriano-WebDev <Valeriano-WebDev@gmail.com>
2022-02-16 13:42:09 -0800

    added answers to first few challenge questions

---
+++ jspractice-jennice-jacob.js
@@ -0,0 +1,13 @@
+console.log(34 + 71)
+console.log(67 - 123)
+console.log(56 * 12)
+console.log(45 / 5)
+console.log(5 ** 7)
+console.log(33 % 6)
+console.log("jacob".length)
+
+
+console.log( 34 / 3 >= 67 / 2)
+console.log(5 === "5")
+console.log(!3 === 3)
+console.log("LEARN".length === 5 && "Student".length === 10)
