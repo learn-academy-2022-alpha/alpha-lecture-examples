5dd9074a3e086ab51b17d8e4fbb7c003c0624a8d
Sarah Proctor <sproctor950@gmail.com>
2022-03-02 11:59:40 -0800

    adds react intro and vocab file, htmls file with a react component, create react app with description in the README

---
+++ our-first-react-project/src/App.js
@@ -0,0 +1,21 @@
+// src/App.js
+
+import React, { Component } from 'react'
+import Footer from './components/Footer'
+
+class App extends Component {
+  render() {
+    return(
+      <>
+        <h1>Hello Alpha!</h1>
+        <h2>Happy React day!</h2>
+        <Footer />
+        <Footer />
+        <Footer />
+        <Footer />
+      </>
+    )
+  }
+}
+
+export default App
