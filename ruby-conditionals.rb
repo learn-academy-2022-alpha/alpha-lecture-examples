39a7c6330b9fad98df754bd8ae3f69eaacf5dcab
Charlean Baxter <baxtercm@outlook.com>
2022-03-09 15:55:31 -0800

    examples of ruby conditionals, variables, and interacting with user inputs

---
+++ ruby-conditionals.rb
@@ -0,0 +1,47 @@
+# Ruby conditionals
+
+# keyword if - takes an evaluation
+# javascript
+# if(2 + 2 === 4) {
+#   return "Quad"
+# }
+
+# ruby
+# if 2 + 2 == 4
+#   p 'Quad'
+# end  
+
+# keyword else the catchall
+# if 2 + 2 == 5
+#   p 'The answer is 4'
+# else
+#   p 'That is not right'
+# end
+
+# keyword elsif
+# if 2 + 2 == 5
+#   p 'Is the answer is 5?'
+# elsif 2 + 2 == 6
+#   p 'Is the answer 6?'
+# else
+#   p 'That is not right'
+# end
+
+# variables
+# string interpolation
+# "#{variable}"
+# my_name = 'Elyse'
+
+# if my_name == 'Charlean'
+#   p 'Aloha, Hola, Charlean!'
+# else 
+#   p "Hi there, #{my_name}"
+# end
+
+# keyword gets - method will stop the execution of the program and wait for you to type something
+# my_name = gets
+# p "#{my_name} has a great beard"
+
+# # .chomp removes extra characters
+# my_name = gets.chomp
+# p "#{my_name} has a great beard"
