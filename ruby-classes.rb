3ebdc18e2514761b0f6156cc2487c97adad67a12
Beau <halebeau3@gmail.com>
2022-03-15 12:13:38 -0700

    cleaned up syntax

--- ruby-classes.rb
+++ ruby-classes.rb
@@ -7,187 +7,187 @@
 # setters and getters methods - set_ and get_
 
 
-# class NetflixShow
-# end
+class NetflixShow
+end
 
-# # p NetflixShow.new
-# #<NetflixShow:0x0000000149045278>
-# class_example = NetflixShow.new
-# p class_example
+# p NetflixShow.new
+#<NetflixShow:0x0000000149045278>
+class_example = NetflixShow.new
+p class_example
+-----------------------------
+
+#making objects
+
+class NetflixShow
+  def set_show_info(title)
+    @title = title
+  end
+end
+
+#First object
+star = NetflixShow.new
+star.set_show_info('Star Trek')
+p star
+#Second object
+no = NetflixShow.new
+no.set_show_info('New Girls')
+p no
+#<NetflixShow:0x000000016004b2b0 @title="Star Trek">
+#<NetflixShow:0x0000000160049cd0 @title="New Girls">
+-----------------------------
+
+#setter
+
+class NetflixShow
+  def set_show_info(title, run_time)
+    @title = title
+    @run_time = run_time
+  end
+end
+
+star = NetflixShow.new
+star.set_show_info('Star Trek', '45min')
+p star
+#<NetflixShow:0x000000012984d300 @title="Star Trek", @run_time="45min">
+-----------------------------
+
+#getter
+
+class NetflixShow
+  def set_show_info(title, run_time)
+    @title = title
+    @run_time = run_time
+  end
+  def get_title
+    @title
+  end
+  def get_run_time
+    @run_time
+  end
+end
+
+star = NetflixShow.new
+star.set_show_info('Star Trek', '45min')
+p star.get_title
+p star.get_run_time
+
+# 
+# "Star Trek"
+# "45min"
 #-----------------------------
 
-##making objects
+#initialize
 
-# class NetflixShow
-#   def set_show_info(title)
-#     @title = title
-#   end
-# end
+class NetflixShow
+  def initialize(title, run_time)
+    @title = title
+    @run_time = run_time
+  end
+  def get_title
+    @title
+  end
+  def get_run_time
+    @run_time
+  end
+end
 
-# #First object
-# star = NetflixShow.new
-# star.set_show_info('Star Trek')
-# p star
-# #Second object
-# no = NetflixShow.new
-# no.set_show_info('New Girls')
-# p no
-# #<NetflixShow:0x000000016004b2b0 @title="Star Trek">
-# #<NetflixShow:0x0000000160049cd0 @title="New Girls">
-#-----------------------------
+star = NetflixShow.new('Star Trek', '45min')
+p star.get_title
+p star.get_run_time
 
-##setter
-
-# class NetflixShow
-#   def set_show_info(title, run_time)
-#     @title = title
-#     @run_time = run_time
-#   end
-# end
-
-# star = NetflixShow.new
-# star.set_show_info('Star Trek', '45min')
-# p star
-# #<NetflixShow:0x000000012984d300 @title="Star Trek", @run_time="45min">
-#-----------------------------
-
-##getter
-
-# class NetflixShow
-#   def set_show_info(title, run_time)
-#     @title = title
-#     @run_time = run_time
-#   end
-#   def get_title
-#     @title
-#   end
-#   def get_run_time
-#     @run_time
-#   end
-# end
-
-# star = NetflixShow.new
-# star.set_show_info('Star Trek', '45min')
-# p star.get_title
-# p star.get_run_time
-
-# # 
-# # "Star Trek"
-# # "45min"
+# "Star Trek"
+# "45min"
 # #-----------------------------
 
-##initialize
+#initialize
 
-# class NetflixShow
-#   def initialize(title, run_time)
-#     @title = title
-#     @run_time = run_time
-#   end
-#   def get_title
-#     @title
-#   end
-#   def get_run_time
-#     @run_time
-#   end
-# end
+class NetflixShow
+  def initialize(title, run_time)
+    @title = title
+    @run_time = run_time
+    @watched = false
+  end
+  def get_title
+    @title
+  end
+  def get_run_time
+    @run_time
+  end
+  def been_watched
+    @watched = true
+  end
+  def show_data
+    if @watched
+      "You have watched the show called #{@title} which is #{@run_time}."
+    else
+      "You have NOT watched the show called #{@title} which is #{@run_time}."
+    end
+  end
+end
 
-# star = NetflixShow.new('Star Trek', '45min')
-# p star.get_title
-# p star.get_run_time
+star = NetflixShow.new('Star Trek', '45min')
+p star.show_data
+star.been_watched
+p star.show_data
 
-# # "Star Trek"
-# # "45min"
-# # #-----------------------------
+# "Star Trek"
+# "45min"
+# "You have NOT watched the show called Star Trek which is 45min."
+# "You have watched the show called Star Trek which is 45min."
+-------------------------------------
 
-##initialize
+#bringing all together
 
-# class NetflixShow
-#   def initialize(title, run_time)
-#     @title = title
-#     @run_time = run_time
-#     @watched = false
-#   end
-#   def get_title
-#     @title
-#   end
-#   def get_run_time
-#     @run_time
-#   end
-#   def been_watched
-#     @watched = true
-#   end
-#   def show_data
-#     if @watched
-#       "You have watched the show called #{@title} which is #{@run_time}."
-#     else
-#       "You have NOT watched the show called #{@title} which is #{@run_time}."
-#     end
-#   end
-# end
+class NetflixShow
+  def initialize(title, run_time)
+    @title = title
+    @run_time = run_time
+  end
+  def get_title
+    @title
+  end
+  def get_run_time
+    @run_time
+  end
+end
 
-# star = NetflixShow.new('Star Trek', '45min')
-# p star.show_data
-# star.been_watched
-# p star.show_data
+star = NetflixShow.new('Star Trek', '45min')
+p star.get_title
+p star.get_run_time
 
-# # "Star Trek"
-# # "45min"
-# # "You have NOT watched the show called Star Trek which is 45min."
-# # "You have watched the show called Star Trek which is 45min."
-#-------------------------------------
+# "Star Trek"
+# "45min"
+# #-----------------------------
 
-##bringing all together
+#refactor
+#attr_accessor
 
-# class NetflixShow
-#   def initialize(title, run_time)
-#     @title = title
-#     @run_time = run_time
-#   end
-#   def get_title
-#     @title
-#   end
-#   def get_run_time
-#     @run_time
-#   end
-# end
+class NetflixShow
+  attr_accessor :title, :run_time, :watched
+  def initialize(title, run_time)
+    @title = title
+    @run_time = run_time
+    @watched = false
+  end
+  def show_data
+    if @watched
+      "You have watched the show called #{@title} which is #{@run_time}."
+    else
+      "You have NOT watched the show called #{@title} which is #{@run_time}."
+    end
+  end
+end
 
-# star = NetflixShow.new('Star Trek', '45min')
-# p star.get_title
-# p star.get_run_time
-
-# # "Star Trek"
-# # "45min"
-# # #-----------------------------
-
-##refactor
-##attr_accessor
-
-# class NetflixShow
-#   attr_accessor :title, :run_time, :watched
-#   def initialize(title, run_time)
-#     @title = title
-#     @run_time = run_time
-#     @watched = false
-#   end
-#   def show_data
-#     if @watched
-#       "You have watched the show called #{@title} which is #{@run_time}."
-#     else
-#       "You have NOT watched the show called #{@title} which is #{@run_time}."
-#     end
-#   end
-# end
-
-# star = NetflixShow.new('Star Trek', '45min')
-# p star.title
-# star.title = 'DS9'
-# p star.show_data
-# p star.run_time = '90min'
-# p star.show_data
-# star.watched = true
-# p star.show_data
-# # "Star Trek"
-# # "You have NOT watched the show called DS9 which is 45min."
-# # "90min"
-# # "You have NOT watched the show called DS9 which is 90min."
-# # "You have watched the show called DS9 which is 90min."
+star = NetflixShow.new('Star Trek', '45min')
+p star.title
+star.title = 'DS9'
+p star.show_data
+p star.run_time = '90min'
+p star.show_data
+star.watched = true
+p star.show_data
+# "Star Trek"
+# "You have NOT watched the show called DS9 which is 45min."
+# "90min"
+# "You have NOT watched the show called DS9 which is 90min."
+# "You have watched the show called DS9 which is 90min."
